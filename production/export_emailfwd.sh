48f4d026d2120cb3658906a0de785efcf06051ea
Matthew Scott <gldnspud@gmail.com>
2010-11-16 15:19:20 -0800

    Also sync emailfwd data

--- production/export_emailfwd.sh
+++ production/export_emailfwd.sh
@@ -3,9 +3,11 @@
 export PYTHONPATH=$HOME
 export DJANGO_SETTINGS_MODULE=snswa_deploy.localsettings
 
-export EMAILFWD_DATA=$HOME/emailfwd_data
+export EMAILFWD_DATA=$HOME/emailfwd-data
 
 mkdir -p ${EMAILFWD_DATA}
 rm ${EMAILFWD_DATA}/*
 
 exec $HOME/src/env/bin/django-admin.py export_emailfwd ${EMAILFWD_DATA}
+
+$HOME/sync-emailfwd-data.sh
