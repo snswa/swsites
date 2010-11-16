5f7bd6a77e064f026445be718a10cdec468093fe
Matthew Scott <gldnspud@gmail.com>
2010-11-16 15:16:55 -0800

    Management command to export email forwarding data

---
+++ emailfwd/management/commands/export_emailfwd.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+
+import os
+import sys
+
+from django.core.management.base import BaseCommand
+
+from emailfwd.models import ForwardedEmailAddress
+
+
+class Command(BaseCommand):
+    args = '<output_dir>'
+    help = 'Export the email forwarding data directory'
+
+    def handle(self, *args, **options):
+        output_dir = args[0]
+        if not os.path.isdir(output_dir) or os.listdir('data'):
+            print('Provide an empty directory that exists', file=sys.stderr)
+            return 1
+        for fwd in ForwardedEmailAddress.objects.all():
+            outname = os.path.join(output_dir, '{0.name}@{0.domain}'.format(fwd))
+            with open(outname, 'wb') as out:
+                for dest in fwd.emaildestination_set.all():
+                    print(dest.email, file=out)
---
+++ production/export_emailfwd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PYTHONPATH=$HOME
+export DJANGO_SETTINGS_MODULE=snswa_deploy.localsettings
+
+export EMAILFWD_DATA=$HOME/emailfwd_data
+
+mkdir -p ${EMAILFWD_DATA}
+rm ${EMAILFWD_DATA}/*
+
+exec $HOME/src/env/bin/django-admin.py export_emailfwd ${EMAILFWD_DATA}
