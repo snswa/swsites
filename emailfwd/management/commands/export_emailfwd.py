d6e9b587644d6b6a8c0d1e11407811c9a0576461
Matthew Scott <gldnspud@gmail.com>
2010-11-16 21:43:26 -0800

    Use output_dir, not hard-coded dir name

--- emailfwd/management/commands/export_emailfwd.py
+++ emailfwd/management/commands/export_emailfwd.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         output_dir = args[0]
-        if not os.path.isdir(output_dir) or os.listdir('data'):
+        if not os.path.isdir(output_dir) or os.listdir(output_dir):
             print('Provide an empty directory that exists', file=sys.stderr)
             return 1
         for fwd in ForwardedEmailAddress.objects.all():
