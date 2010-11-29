9a86a15b4defa8a1ee83425b885f70cc191b12c7
Matthew Scott <gldnspud@gmail.com>
2010-11-29 00:04:47 -0800

    ditch _view suffix on view function

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'dashboard.views',
-    url(r'^$', 'index_view'),
+    url(r'^$', 'index'),
 )
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
 
-def index_view(request):
+def index(request):
     template_name = 'dashboard/index.html'
     template_context = {}
     return render_to_response(
