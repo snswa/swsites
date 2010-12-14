2a7db5d1b4ba8b0422d076c92866072e4fadb7cc
Matthew Scott <gldnspud@gmail.com>
2010-12-14 15:29:02 -0800

    Add a number of icons to links, esp. in breadcrumbs, to differentiate different types of locations.

--- sw/templatetags/breadcrumbs.py
+++ sw/templatetags/breadcrumbs.py
@@ -27,11 +27,23 @@ def breadcrumb(parser, token):
 
     @author Andriy Drozdyuk
     """
-    return BreadcrumbNode(token.split_contents()[1:])
+    bits = token.split_contents()
+    # Insert empty CSS class.
+    bits.insert(1, '""')
+    token.contents = ' '.join(bits)
+    bits = token.split_contents()[1:]
+    return BreadcrumbNode(bits)
 
 
 @register.tag
-def breadcrumb_url(parser, token):
+def breadcrumb_css(parser, token):
+    """Same as breadcrumb, except specify a CSS class in front."""
+    bits = token.split_contents()[1:]
+    return BreadcrumbNode(bits)
+
+
+@register.tag
+def breadcrumb_url(parser, token, css_class='""'):
     """
     Same as breadcrumb
     but instead of url context variable takes in all the
@@ -47,75 +59,104 @@ def breadcrumb_url(parser, token):
     # Extract our extra title parameter
     title = bits.pop(1)
     token.contents = ' '.join(bits)
-
+    # Shove it back into to the token to url parse it.
     url_node = url(parser, token)
 
-    return UrlBreadcrumbNode(title, url_node)
+    return UrlBreadcrumbNode(css_class, title, url_node)
+
+
+@register.tag
+def breadcrumb_url_css(parser, token):
+    """Same as breadcrumb_url, except specify a CSS class in front."""
+    bits = token.split_contents()
+    css_class = bits.pop(1)
+    print 'css_class', css_class, 'bits', bits
+    token.contents = ' '.join(bits)
+    return breadcrumb_url(parser, token, css_class)
 
 
 class BreadcrumbNode(Node):
     def __init__(self, vars):
-        """
-        First var is title, second var is url context variable
-        """
         self.vars = map(Variable,vars)
+        # 0 -> css class
+        # 1 -> title
+        # 2 -> url
 
     def render(self, context):
-        title = self.vars[0].var
-
-        if title.find("'")==-1 and title.find('"')==-1:
+        css_class = self.vars[0].var
+        if css_class.find("'") == -1 and css_class.find('"') == -1:
             try:
                 val = self.vars[0]
+                css_class = val.resolve(context)
+            except:
+                css_class = ''
+        else:
+            css_class = css_class.strip("'").strip('"')
+            css_class = smart_unicode(css_class)
+        #
+        title = self.vars[1].var
+        if title.find("'") == -1 and title.find('"') == -1:
+            try:
+                val = self.vars[1]
                 title = val.resolve(context)
             except:
                 title = ''
-
         else:
-            title=title.strip("'").strip('"')
-            title=smart_unicode(title)
-
+            title = title.strip("'").strip('"')
+            title = smart_unicode(title)
+        #
         url = None
-
-        if len(self.vars)>1:
-            val = self.vars[1]
+        if len(self.vars)>2:
+            val = self.vars[2]
             try:
                 url = val.resolve(context)
             except VariableDoesNotExist:
                 print 'URL does not exist', val
                 url = None
-
-        return create_crumb(title, url)
+        #
+        return create_crumb(css_class, title, url)
 
 
 class UrlBreadcrumbNode(Node):
-    def __init__(self, title, url_node):
+    def __init__(self, css_class, title, url_node):
+        self.css_class = Variable(css_class)
         self.title = Variable(title)
         self.url_node = url_node
 
     def render(self, context):
         title = self.title.var
-
-        if title.find("'")==-1 and title.find('"')==-1:
+        if title.find("'") == -1 and title.find('"') == -1:
             try:
                 val = self.title
                 title = val.resolve(context)
             except:
                 title = ''
         else:
-            title=title.strip("'").strip('"')
-            title=smart_unicode(title)
-
+            title = title.strip("'").strip('"')
+            title = smart_unicode(title)
+        #
+        css_class = self.css_class.var
+        if css_class.find("'") == -1 and css_class.find('"') == -1:
+            try:
+                val = self.css_class
+                css_class = val.resolve(context)
+            except:
+                css_class = ''
+        else:
+            css_class = css_class.strip("'").strip('"')
+            css_class = smart_unicode(css_class)
+        #
         url = self.url_node.render(context)
-        return create_crumb(title, url)
+        return create_crumb(css_class, title, url)
 
 
-def create_crumb(title, url=None):
+def create_crumb(css_class, title, url):
     """
     Helper function
     """
     if url:
-        crumb = "<li><a href='%s'>%s</a></li>" % (url, title)
+        crumb = "<li><span class='{css_class}'><a href='{url}'><span>{title}</span></a></span></li>".format(**locals())
     else:
-        crumb = "<li>%s</li>" % (title,)
+        crumb = "<li><span class='{css_class}'><span>{title}</span></span></li>".format(**locals())
 
     return crumb
