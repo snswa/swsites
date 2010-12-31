a2827c46e9f4f0f9bfc6cbab7463ea3b5dba856b
Matthew Scott <gldnspud@gmail.com>
2010-12-30 17:20:56 -0800

    Better production configuration for celeryd

--- production/celeryd.sh
+++ production/celeryd.sh
@@ -4,5 +4,10 @@ export PYTHONPATH=$HOME
 export DJANGO_SETTINGS_MODULE=snswa_deploy.localsettings
 
 exec $HOME/src/env/bin/django-admin.py celeryd \
-    --settings=snswa_deploy.localsettings \
-    -c1 -B -E -linfo
+  --settings=snswa_deploy.localsettings \
+  --concurrency=5 \
+  --beat \
+  --schedule=$HOME/celerybeat-schedule \
+  --events \
+  --logfile=$HOME/logs/celeryd.log \
+  --loglevel=info
