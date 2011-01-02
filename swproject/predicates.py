f04e00b50261e7ec7871ab33beb5a62f4a88b20d
Matthew Scott <gldnspud@gmail.com>
2011-01-01 17:39:44 -0800

    Allow members of private teams to edit events.

--- swproject/predicates.py
+++ swproject/predicates.py
@@ -1,7 +1,14 @@
+from groups.templatetags.group_tags import ismemberofgroup
 from teams.templatetags.team_tags import iscoordinatorofteam
 
 
 def event_edit(request, event):
-    return iscoordinatorofteam(request.user, request.group)
+    group = request.group
+    user = request.user
+    if iscoordinatorofteam(user, group):
+        return True
+    if getattr(group, 'is_private', False) and ismemberofgroup(user, group):
+        return True
+    return False
 
 event_delete = event_edit
