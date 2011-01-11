3bded2e0d247d527ee60b87d9d64c3ff761337b6
Matthew Scott <gldnspud@gmail.com>
2011-01-10 20:28:17 -0800

    Show icons next to iris content links.

---
+++ sw/templatetags/contenttype_tags.py
@@ -0,0 +1,11 @@
+from django.contrib.contenttypes.models import ContentType
+from django.template import Library
+
+
+register = Library()
+
+
+@register.filter
+def contenttypecssname(obj):
+    ct = ContentType.objects.get_for_model(obj)
+    return u'{0}-{1}'.format(ct.app_label, ct.model)
