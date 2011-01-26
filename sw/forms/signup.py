29b93a421016e5cf1007a047c0fb744eb5377c7d
Matthew Scott <gldnspud@gmail.com>
2011-01-26 15:16:29 -0800

    Show friendly error messages when username already in use or when username contains invalid characters.

--- sw/forms/signup.py
+++ sw/forms/signup.py
@@ -2,6 +2,7 @@ import re
 
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from django import forms
 from django.utils.translation import ugettext_lazy as _, ugettext
 
@@ -11,13 +12,43 @@ from sw.models import Profile
 from sw.decorators import autostrip
 
 
+def username_not_in_use(username):
+    if User.objects.filter(username__iexact=username).exists():
+        raise forms.ValidationError('This username is already in use.')
+
+
 @autostrip
 class HqSignupForm(SignupForm):
 
+    # Needs to be new_ prefixed so as to not collide with a login form on
+    # the same page.
+    new_username = forms.RegexField(
+        label=_("Username"),
+        help_text=_('The account name you will use to log into the website.'),
+        max_length=30,
+        regex=r'^[\w\._-]+$',
+        error_messages=dict(
+            invalid='Your username may not contain spaces. Use only letters, numbers, periods, underscores, and dashes.',
+        ),
+        validators=[
+            username_not_in_use,
+        ],
+        widget=forms.TextInput(),
+    )
+    password1 = forms.CharField(
+        label=_("Password"),
+        help_text=_('Choose a strong password to help prevent identity theft.'),
+        widget=forms.PasswordInput(render_value=False),
+    )
+    password2 = forms.CharField(
+        label=_("Password (again)"),
+        widget=forms.PasswordInput(render_value=False),
+    )
     preferred_name = forms.CharField(
         label=_("Preferred Name"),
-        help_text=_('How your name will appear to other volunteers. Example: Your first name, or a personal nickname.'),
+        help_text=_('How your name will appear to other volunteers. Example: Your first name, or a personal nickname. Leave blank to use your username.'),
         max_length=50,
+        required=False,
         widget=forms.TextInput(),
     )
     zip_code = forms.CharField(
@@ -32,40 +63,23 @@ class HqSignupForm(SignupForm):
     email_delivery = forms.ChoiceField(choices=Profile.EMAIL_DELIVERY_CHOICES, initial='00/48')
     phone_number = forms.CharField(
         label=_("Phone Number"),
-        help_text=_('Strongly preferred.'),
+        help_text=_('Strongly preferred. Available to coordinators only until you change privacy settings.'),
         max_length=20,
         widget=forms.TextInput(),
         required=False,
-    )
-    # Needs to be new_ prefixed so as to not collide with a login form on
-    # the same page.
-    new_username = forms.CharField(
-        label=_("Username"),
-        help_text=_('The account name you will use to log into the website.'),
-        max_length=30,
-        widget=forms.TextInput(),
-    )
-    password1 = forms.CharField(
-        label=_("Password"),
-        help_text=_('Choose a strong password to help prevent identity theft.'),
-        widget=forms.PasswordInput(render_value=False),
-    )
-    password2 = forms.CharField(
-        label=_("Password (again)"),
-        widget=forms.PasswordInput(render_value=False),
     )
 
     def __init__(self, *args, **kwargs):
         super(HqSignupForm, self).__init__(*args, **kwargs)
         self.fields.keyOrder = [
+            'new_username',
+            'password1',
+            'password2',
             'preferred_name',
             'zip_code',
             'email',
             'email_delivery',
             'phone_number',
-            'new_username',
-            'password1',
-            'password2',
         ]
 
     def clean(self):
@@ -74,17 +88,14 @@ class HqSignupForm(SignupForm):
             self.cleaned_data['username'] = self.cleaned_data['new_username']
         return self.cleaned_data
 
-    def clean_new_username(self):
-        new_username = self.cleaned_data['new_username']
-        user_exists = User.objects.filter(username__iexact=new_username).exists()
-        if user_exists:
-            raise forms.ValidationError('This username is already in use.')
-        return new_username
-
     def after_signup(self, user, **kwargs):
         # Put everything into the user's profile.
+        preferred_name = self.cleaned_data['preferred_name']
         profile = user.get_profile()
-        profile.preferred_name = self.cleaned_data['preferred_name']
+        if preferred_name:
+            profile.preferred_name = preferred_name
+        else:
+            profile.preferred_name = self.cleaned_data['new_username']
         profile.zip_code = self.cleaned_data['zip_code']
         profile.phone_number = self.cleaned_data['phone_number']
         profile.email_delivery = self.cleaned_data['email_delivery']
