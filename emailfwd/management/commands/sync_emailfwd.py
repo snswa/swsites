0ba2f19832d5f110a6535c5a87b22db2c9cdb61f
Matthew Scott <gldnspud@gmail.com>
2011-02-13 17:23:34 -0800

    Avoid updating configuration when no changes have occurred.

--- emailfwd/management/commands/sync_emailfwd.py
+++ emailfwd/management/commands/sync_emailfwd.py
@@ -15,19 +15,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         server = xmlrpclib.ServerProxy('https://api.webfaction.com/')
+        #
+        # Sign in.
         session_id, account = server.login(settings.WEBFACTION_USERNAME, settings.WEBFACTION_PASSWORD)
+        #
+        # Determine existing email addresses.
         emails = server.list_emails(session_id)
-        existing_addresses = set(email['email_address'] for email in emails if email['email_address'][0] not in '@_')
+        existing_addresses = dict((info['email_address'], info) for info in emails if info['email_address'][0] not in '@_')
+        #
+        # Determine desired configuration.
         desired_configuration = {}
         desired_addresses = set()
+        #
         for fwd in ForwardedEmailAddress.objects.all():
             name = fwd.name.lower()
             domain = fwd.domain.lower()
             email = '{0}@{1}'.format(name, domain)
             desired_configuration[email] = fwd
             desired_addresses.add(email)
-        delete_addresses = existing_addresses - desired_addresses
-        create_addresses = desired_addresses - existing_addresses
+        delete_addresses = set(existing_addresses) - desired_addresses
+        create_addresses = desired_addresses - set(existing_addresses)
         update_addresses = desired_addresses - create_addresses
         for address in create_addresses:
             config = desired_configuration[address]
@@ -37,8 +44,9 @@ class Command(BaseCommand):
         for address in update_addresses:
             config = desired_configuration[address]
             targets = ','.join(dest.email for dest in config.emaildestination_set.all())
-            print('updating email={0} targets={1}'.format(address, targets))
-            server.update_email(session_id, address, targets)
+            if targets != existing_addresses[address]['targets']:
+                print('updating email={0} targets={1}'.format(address, targets))
+                server.update_email(session_id, address, targets)
         for address in delete_addresses:
             print('deleting email={0}'.format(address))
             server.delete_email(session_id, address)
