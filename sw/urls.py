3b8799e5b45519e108773561913596acb7bf629a
Matthew Scott <gldnspud@gmail.com>
2011-02-25 12:14:16 -0800

    Turn sw.views into a package; Move sw.views module to sw.views.__init__

--- sw/urls.py
+++ sw/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 
-urlpatterns = patterns('sw.views',
-    url(r'^deploy_tests/report_ok$', 'report_ok_view'),
+urlpatterns = patterns('',
+    url(r'^deploy_tests/report_ok$', 'sw.views.report_ok_view'),
 )
